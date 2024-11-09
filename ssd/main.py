db3a6304b520a5d5501136ea01d4f78253b48098
mbyhphat <22521067@gm.uit.edu.vn>
2024-11-09 23:04:16 +0700

    Temp update main file

--- ssd/main.py
+++ ssd/main.py
@@ -5,6 +5,8 @@ from torchvision import transforms
 import albumentations as A
 from torchvision.models.detection import ssd300_vgg16
 from torch.utils.data import Dataset, DataLoader
+import sys
+sys.path.append(r'E:\Traffic-Vehicle-Detection-in-Normal-and-Adverse-Environmental-Conditions')
 from dataloader import TrafficVehicle
 import torch.optim as optim
 
@@ -14,12 +16,17 @@ transform = A.Compose([
     A.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
 ], bbox_params=A.BboxParams(format='pascal_voc', label_fields=['class_labels']))
 
-train_dataset = TrafficVehicle(r'E:\Vehicle Detection\train', transforms=transform)
+train_dataset = TrafficVehicle(r'E:\Vehicle Detection\train', transforms=transform, transform_box_type="corner")
 train_loader = DataLoader(train_dataset, batch_size=8, shuffle=False, collate_fn=lambda x: tuple(zip(*x))) 
 # collate_fn=lambda x: tuple(zip(*x)): change from [(data1, label1), (data2, label2), (data3, label3)] to ((data1, data2, data3), (label1, label2, label3))
 
+# for images, targets in train_loader:
+#     for image in images:
+#         print(image.shape)
+#     break
+
 model = ssd300_vgg16(pretrained=True)
-num_classes = 4
+num_classes = 5
 model.head.classification_head.num_classes = num_classes
 
 optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9, weight_decay=5e-4)
