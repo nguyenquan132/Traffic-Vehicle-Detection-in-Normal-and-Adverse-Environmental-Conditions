e866d358dd992f98cd782dd085a339ec7d8bf2f2
QuanNguyen123 <128344377+nguyenquan132@users.noreply.github.com>
2024-11-10 21:17:14 +0700

    Sửa file evaluate.py

--- faster_rcnn/evaluate.py
+++ faster_rcnn/evaluate.py
@@ -6,35 +6,61 @@ import torchvision.ops as ops
 
 def evaluate(val_dataloader: DataLoader,
               model: torchvision.models,
+              num_class: int,
+              iou_threshold: float,
               device: torch.device):
     model = model.to(device)
     model.eval()
-    precisions = []
-    recalls = []
+    precisions_per_class = {i: [] for i in range(1, num_class + 1)}
+    recalls_per_class = {i: [] for i in range(1, num_class + 1)}
 
     with torch.inference_mode():
         for images, targets in val_dataloader:
             image = list(image.to(device) for image in images)
             target = [{k: v.to(device) for k, v in t.items()} for t in targets]
 
-            output = model(image)
+            outputs = model(image)
 
-            for i, target in enumerate(target):
-                true_boxes = target['box'].cpu().detach().numpy()
-                pred_boxes = output[i]['boxes'].cpu().detach().numpy()
-                scores = output[i]['scores'].cpu().detach().numpy()
+            for img_idx, (target, output) in enumerate(zip(target, outputs)):
+                true_boxes = target['boxes'].cpu().detach().numpy()
+                true_labels = target['labels'].cpu().detach().numpy()
+                
+                pred_boxes = output['boxes'].cpu().detach().numpy()
+                pred_scores = output['scores'].cpu().detach().numpy()
+                pred_labels = output['labels'].cpu().detach().numpy()
 
                 # Áp dụng NMS để loại bỏ các box dư thừa
-                keep = ops.nms(pred_boxes, scores, iou_threshold=0.5)
+                keep = ops.nms(torch.from_numpy(pred_boxes),
+                               torch.from_numpy(pred_scores),
+                               iou_threshold=iou_threshold)
                 pred_boxes = pred_boxes[keep]
-                scores = scores[keep]
+                pred_scores = pred_scores[keep]
+                pred_labels = pred_labels[keep]
 
-                precision, recall = calculate_precision_recall(true_boxes=true_boxes, pred_boxes=pred_boxes, iou_threshold=0.5)
-                precisions.append(precision)
-                recalls.append(recall)
+                for class_id in range(1, num_class + 1):
+                    # Lọc true và predicted boxes theo class_id
+                    true_boxes_class = true_boxes[true_labels == class_id]
+                    pred_boxes_class = pred_boxes[pred_labels == class_id]
+                    pred_scores_class = pred_scores[pred_labels == class_id]
 
-        AP = calculate_ap(precisions=precisions, recalls=recalls)
+                    precision, recall = calculate_precision_recall(true_boxes=true_boxes_class,
+                                                                   pred_boxes=pred_boxes_class,
+                                                                   iou_threshold=iou_threshold)
+                    
+                    precisions_per_class[class_id].append(precision)
+                    recalls_per_class[class_id].append(recall)
 
-        return AP
+
+        AP_per_class = {}
+        for class_id in range(1, num_class + 1):
+            precisions = precisions_per_class[class_id]
+            recalls = recalls_per_class[class_id]
+
+            # Tính AP cho class này
+            AP_per_class[class_id] = calculate_ap(precisions=precisions, recalls=recalls)
+
+        mAP = sum(AP_per_class.values) / num_class
+        
+        return mAP, precisions_per_class, recalls_per_class
             
 
