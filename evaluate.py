f969cc65b920cff8f31f3f37f2c2a074007d1c0c
QuanNguyen123 <128344377+nguyenquan132@users.noreply.github.com>
2024-11-13 15:28:31 +0700

    evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -49,8 +49,8 @@ def evaluate(val_dataloader: DataLoader,
                                                                    pred_boxes=pred_boxes_class,
                                                                    iou_threshold=iou_threshold)
                     
-                    print(len(precision))
-                    print(len(precisions_per_class))
+                    print(precision)
+                    print(precisions_per_class)
                     
                     # Lưu precision, recall và confidence scores cho từng class
                     precisions_per_class[class_id].append(precision)
