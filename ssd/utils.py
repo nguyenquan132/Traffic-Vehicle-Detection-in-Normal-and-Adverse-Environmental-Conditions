f8979dcd1d261b28b2babb2cdd13f00ea5daa5c7
mbyhphat <22521067@gm.uit.edu.vn>
2024-11-14 15:44:53 +0700

    Delete save model function

--- ssd/utils.py
+++ ssd/utils.py
@@ -20,31 +20,3 @@ class Averager:
     def reset(self):
         self.current_total = 0.0
         self.iterations = 0.0
-
-class SaveBestModel:
-    """
-    Class to save the best model while training. If the current epoch's 
-    validation mAP @0.5:0.95 IoU higher than the previous highest, then save the
-    model state.
-    """
-    def __init__(
-        self, best_valid_map=float(0)
-    ):
-        self.best_valid_map = best_valid_map
-        
-    def __call__(
-        self, 
-        model, 
-        current_valid_map, 
-        epoch, 
-        OUT_DIR,
-    ):
-        if current_valid_map > self.best_valid_map:
-            self.best_valid_map = current_valid_map
-            print(f"\nBEST VALIDATION mAP: {self.best_valid_map}")
-            print(f"\nSAVING BEST MODEL FOR EPOCH: {epoch+1}\n")
-            torch.save({
-                'epoch': epoch+1,
-                'model_state_dict': model.state_dict(),
-                }, f"{OUT_DIR}/best_model.pth")
-            
