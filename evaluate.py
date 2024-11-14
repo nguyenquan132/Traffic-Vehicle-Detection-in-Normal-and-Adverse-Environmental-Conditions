5b2cb8646ef5edcccb77f5b3448ddfc800f001d8
QuanNguyen123 <128344377+nguyenquan132@users.noreply.github.com>
2024-11-14 23:40:17 +0700

    update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -15,34 +15,30 @@ def evaluate(val_dataloader: DataLoader,
 
     precisions_per_class = {i: [] for i in range(1, num_class + 1)}
     recalls_per_class = {i: [] for i in range(1, num_class + 1)}
-    confidence_per_class = {i: [] for i in range(1, num_class + 1)}
     ap_per_class = {i: [] for i in range(1, num_class + 1)}
 
     with torch.inference_mode():
-        for images, targets in tqdm(val_dataloader, total=len(val_dataloader)):
+        for batch, (images, targets) in tqdm(enumerate(val_dataloader), total=len(val_dataloader)):
             image = list(image.to(device) for image in images)
             target = [{k: v.to(device) for k, v in t.items()} for t in targets]
 
             outputs = model(image)
 
-            batch_ap, batch_precision, batch_recall, batch_confidence = evaluate_predictions(predictions=outputs,
-                                                                                             targets=target,
-                                                                                             num_class=num_class,
-                                                                                             iou_threshold=iou_threshold)
+            batch_ap, batch_precision, batch_recall = evaluate_predictions(predictions=outputs,
+                                                                           targets=target,
+                                                                           num_class=num_class,
+                                                                           iou_threshold=iou_threshold)
 
             # Cập nhật AP, precision, recall, confidence cho từng class
             for class_id in range(1, num_class + 1):
                 ap_per_class[class_id].extend(batch_ap[class_id])
                 
-                # Chỉ gọi np.concatenate nếu có giá trị
                 if batch_precision[class_id]:
                     precisions_per_class[class_id].extend(np.concatenate(batch_precision[class_id]))
                 if batch_recall[class_id]:
                     recalls_per_class[class_id].extend(np.concatenate(batch_recall[class_id]))
-                if batch_confidence[class_id]:
-                    confidence_per_class[class_id].extend(np.concatenate(batch_confidence[class_id]))
 
-        # Tính AP cho mỗi class
+        # Tính mean AP cho mỗi class
         AP_per_class = {
             class_id: np.mean(aps) if len(aps) > 0 else 0
             for class_id, aps in ap_per_class.items()
@@ -53,4 +49,4 @@ def evaluate(val_dataloader: DataLoader,
         
 
 
-        return mAP, AP_per_class, precisions_per_class, recalls_per_class, confidence_per_class
+        return mAP, AP_per_class, precisions_per_class, recalls_per_class
