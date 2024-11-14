a4c239a988bac9e3c9d0ac09716858543e536acc
mbyhphat <22521067@gm.uit.edu.vn>
2024-11-14 15:49:02 +0700

    Update training process and add early stopping

--- ssd/train.py
+++ ssd/train.py
@@ -9,6 +9,7 @@ from torch.optim.lr_scheduler import StepLR
 from albumentations.pytorch import ToTensorV2
 from .utils import * 
 from evaluate import evaluate
+from torchmetrics.detection.mean_ap import MeanAveragePrecision
 
 DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
@@ -39,66 +40,110 @@ def train(train_data_loader, model):
         prog_bar.set_description(desc=f"Loss: {loss_value:.4f}")
     return loss_value
 
+def validate(valid_data_loader, model):
+    print('Validating')
+    model.eval()
+    
+    # Initialize tqdm progress bar.
+    prog_bar = tqdm(valid_data_loader, total=len(valid_data_loader))
+    target = []
+    preds = []
+    for i, data in enumerate(prog_bar):
+        images, targets = data
+        
+        images = list(image.to(DEVICE) for image in images)
+        targets = [{k: v.to(DEVICE) for k, v in t.items()} for t in targets]
+        
+        with torch.no_grad():
+            outputs = model(images, targets)
+
+        # For mAP calculation using Torchmetrics.
+        #####################################
+        for i in range(len(images)):
+            true_dict = dict()
+            preds_dict = dict()
+            true_dict['boxes'] = targets[i]['boxes'].detach().cpu()
+            true_dict['labels'] = targets[i]['labels'].detach().cpu()
+            preds_dict['boxes'] = outputs[i]['boxes'].detach().cpu()
+            preds_dict['scores'] = outputs[i]['scores'].detach().cpu()
+            preds_dict['labels'] = outputs[i]['labels'].detach().cpu()
+            preds.append(preds_dict)
+            target.append(true_dict)
+        #####################################
+
+    metric = MeanAveragePrecision()
+    metric.update(preds, target)
+    metric_summary = metric.compute()
+    return metric_summary
+
 if __name__ == '__main__':
-    transform = A.Compose([
+    train_transform = A.Compose([
         A.Resize(300, 300),
         #A.HorizontalFlip(p=0.5),
         #A.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
-        A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], max_pixel_value=255.0),  # Normalize for ImageNet
+        A.ToFloat(max_value=255.0),
         ToTensorV2(p=1.0),  # Convert to tensor
     ], bbox_params=A.BboxParams(format='pascal_voc', label_fields=['class_labels']))
 
-    train_data_path = os.path.join(os.path.dirname(__file__), '..', 'data/train')
-    train_dataset = TrafficVehicle(train_data_path, transforms=transform, transform_box_type="corner")
+    val_transform = A.Compose([
+            A.Resize(300, 300),
+            A.ToFloat(max_value=255.0),
+            ToTensorV2(p=1.0),
+        ], bbox_params=A.BboxParams(format='pascal_voc', label_fields=['class_labels']))
+
+    train_data_path = '/kaggle/input/traffic-vehicle-detection/data/data/train'
+    train_dataset = TrafficVehicle(train_data_path, transforms=train_transform, transform_box_type="corner")
     train_loader = DataLoader(train_dataset, batch_size=32, shuffle=False, collate_fn=lambda x: tuple(zip(*x)))
 
-    val_data_path = os.path.join(os.path.dirname(__file__), '..', 'data/val')
-    val_dataset = TrafficVehicle(val_data_path, transforms=None, transform_box_type="corner")
+    val_data_path = '/kaggle/input/traffic-vehicle-detection/data/data/val'
+    val_dataset = TrafficVehicle(val_data_path, transforms=val_transform, transform_box_type="corner")
     val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False, collate_fn=lambda x: tuple(zip(*x))) 
-    # collate_fn=lambda x: tuple(zip(*x)): change from [(data1, label1), (data2, label2), (data3, label3)] to ((data1, data2, data3), (label1, label2, label3))
-    
-    # for i in range(1):
-    #     images, targets = next(iter(train_loader))
-    #     images = list(image.to(DEVICE) for image in images)
-    #     targets = [{k: v.to(DEVICE) for k, v in t.items()} for t in targets]
-    #     boxes = targets[i]['boxes'].cpu().numpy().astype(np.int32)
-    #     sample = images[i].permute(1, 2, 0).cpu().numpy()
-    #     sample = cv2.cvtColor(sample, cv2.COLOR_RGB2BGR)
-    #     for box in boxes:
-    #         cv2.rectangle(sample, (box[0],box[1]), (box[2],box[3]), (0,0,255),2)
-    #         #print(box)
-    #     cv2.imshow('img', sample)
-    #     cv2.waitKey(0)
-    #     cv2.destroyAllWindows()
 
     model = create_model(num_classes=5)
     model = model.to(DEVICE)
     params = [p for p in model.parameters() if p.requires_grad]
-    optimizer = torch.optim.SGD(params, lr=0.0001, momentum=0.9, nesterov=True)
+    optimizer = torch.optim.SGD(params, lr=0.001, momentum=0.9, nesterov=True)
     scheduler = StepLR(
-        optimizer=optimizer, step_size=15, gamma=0.1, verbose=True
+        optimizer=optimizer, step_size=10, gamma=0.1, verbose=True
     )
-    save_best_model = SaveBestModel()
-    num_epoch = 20
-
-    # To monitor training loss
+    num_epoch = 40
     train_loss_hist = Averager()
+
     # To store training loss and mAP values.
     train_loss_list = []
-    map_50_list = []
+    map_list = []
+    map_per_class_list = []
+    best_valid_map = 0
+    patience = 4
+    patience_counter = 0
 
     for epoch in range(num_epoch):
         print(f"\nEPOCH {epoch+1} / {num_epoch}")
         train_loss_hist.reset()
         train_loss = train(train_loader, model)
-        val_metric = evaluate(val_dataloader=val_loader, model=model, num_class=5, iou_threshold=0.5, device=DEVICE)
+        val_metric = validate(val_loader, model)
         print(f"Epoch #{epoch+1} train loss: {train_loss_hist.value:.3f}")   
-        print(f"Epoch #{epoch+1} mAP: {val_metric['mAP']}")
+        print(f"Epoch #{epoch+1} mAP: {val_metric['map']}")
 
         train_loss_list.append(train_loss)
-        map_50_list.append(val_metric['mAP'])
-
-        save_best_model(
-            model, float(val_metric['map']), epoch, 'outputs'
-        )
+        map_list.append(val_metric['map'])
+        map_per_class_list.append(val_metric['map_per_class'])
+        current_valid_map = val_metric['map']
+        
+        if current_valid_map > best_valid_map:
+            best_valid_map = current_valid_map
+            patience_counter = 0
+            print(f"\nBEST VALIDATION mAP: {best_valid_map}")
+            print(f"\nSAVING BEST MODEL FOR EPOCH: {epoch+1}\n")
+            torch.save({
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'scheduler_state_dict': scheduler.state_dict(),
+                }, "best_model.pth")
+        else:
+            patience_counter += 1
+            print(f"No improvement in map score for {patience_counter} epochs.")
+        if patience_counter >= patience:
+            print("Early stopping triggered. No improvement in map score.")
+            break
         scheduler.step()
