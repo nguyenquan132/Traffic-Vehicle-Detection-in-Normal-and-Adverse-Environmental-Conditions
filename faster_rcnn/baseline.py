d1d8aa40540dc23e616c4f88130d7ffb9cf48c8d
QuanNguyen123 <128344377+nguyenquan132@users.noreply.github.com>
2024-11-15 16:24:15 +0700

    update baseline.py

--- faster_rcnn/baseline.py
+++ faster_rcnn/baseline.py
@@ -14,23 +14,23 @@ arg = argparse.ArgumentParser(description="Các tham số truyền vào")
 arg.add_argument("--epoch", type=int, default=10, help="Số lượng epoch cho training")
 arg.add_argument("--momentum", type=float, default=0, help="Hệ số momentum cho optimizer")
 arg.add_argument("--weight_decay", type=float, default=0, help="Hệ số weight decay cho optimizer")
-arg.add_argument("--mode", type=str, default="train", help="Trạng thái cho model train hoặc test")
+arg.add_argument("--mode", type=str, default="train", help="Trạng thái cho model train hoặc test hoặc test one image")
 
 parse = arg.parse_args()
 
 train_transform = A.Compose([
     A.Resize(300, 300),
-    A.normalize(mean=0.0, std=1.0),
+    A.Normalize(mean=0.0, std=1.0),
     ToTensorV2()
 ], bbox_params=A.BboxParams(format='pascal_voc', label_fields=['class_labels']))
 
 if __name__ == '__main__':
+
+    # Khởi tạo model
+    weights = models.detection.FasterRCNN_ResNet50_FPN_Weights.DEFAULT
+    model = models.detection.fasterrcnn_resnet50_fpn(weights=weights)
     
     if parse.model == "train":
-        # Khởi tạo model
-        weights = models.detection.FasterRCNN_ResNet50_FPN_Weights.DEFAULT
-        model = models.detection.fasterrcnn_resnet50_fpn(weights=weights)
-
         MODE = TrafficModel(model=model)
 
         train_data = TrafficVehicle(folder="train", transforms=train_transform, transform_box_type="corner")
@@ -48,6 +48,9 @@ if __name__ == '__main__':
                    weight_decay=parse.weight_decay)
 
     if parse.model == "test":
+        pass
+
+    if parse.model == "test one image":
         pass
 
     
