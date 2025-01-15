dcde4c18335305b3e40dc66c5ddba73f4c121b96
QuanNguyen123 <128344377+nguyenquan132@users.noreply.github.com>
2025-01-15 15:38:42 +0700

    update main.py

--- faster_rcnn/main.py
+++ faster_rcnn/main.py
@@ -4,10 +4,11 @@ from torch.utils.data import DataLoader
 import albumentations as A 
 from dataloader import TrafficVehicle
 import torch
-from torchvision import models
+import torchvision
 from albumentations.pytorch import ToTensorV2
 from .model import TrafficModel
-from torch import nn
+from torchvision.models.detection.backbone_utils import BackboneWithFPN
+from torchvision.models.detection import FasterRCNN
 from PIL import Image
 import numpy as np
 
@@ -26,35 +27,51 @@ parse = arg.parse_args()
 
 train_transform = A.Compose([
     A.Resize(300, 300),
-    A.Normalize(mean=0.0, std=1.0),
-    ToTensorV2()
-], bbox_params=A.BboxParams(format='pascal_voc', label_fields=['class_labels']))
+    A.GaussianBlur(blur_limit=(3, 7), p=0.5),
+    A.RandomBrightnessContrast(brightness_limit=(-0.3, -0.3), contrast_limit=(0.2, 0.2), p=0.5),
+    A.HueSaturationValue(hue_shift_limit=(1, 1), p=0.5),
+    A.Sharpen(p=0.5),
+    A.ToFloat(max_value=255.0),
+    ToTensorV2(p=1.0)
+], bbox_params=A.BboxParams(format='pascal_voc', label_fields=['class_labels'], min_visibility=0.3))
 
 test_transform = A.Compose([
-    A.Normalize(mean=0, std=1.0),
-    ToTensorV2()
-])
+    A.Resize(300, 300),
+    A.ToFloat(max_value=255.0),
+    ToTensorV2(p=1.0)
+], bbox_params=A.BboxParams(format='pascal_voc', label_fields=['class_labels']))
 
 if __name__ == '__main__':
 
     # Khởi tạo model
-    weights = models.detection.FasterRCNN_ResNet50_FPN_Weights.DEFAULT
-    model = models.detection.fasterrcnn_resnet50_fpn(weights=weights)
+    weights = torchvision.models.ResNet50_Weights.DEFAULT
+    resnet = torchvision.models.resnet50(weights=weights)
 
-    # change output of classifier layer
-    in_features = model.roi_heads.box_predictor.cls_score.in_features
-    model.roi_heads.box_predictor.cls_score = nn.Linear(in_features=in_features, out_features=5)
-    model.roi_heads.box_predictor.bbox_pred = nn.Linear(in_features=in_features, out_features=5*4)
+    # Xác định returned layers
+    return_layers = {'layer1': '0', 'layer2': '1',  'layer3': '2',  'layer4': '3' }
+
+    # Tạo backbone với FPN
+    backbone = BackboneWithFPN(
+        resnet,
+        return_layers=return_layers,
+        in_channels_list=[256, 512, 1024, 2048],
+        out_channels=256
+    )
+
+    # Khởi tạo FasterRCNN
+    num_classes = 5
+    model = FasterRCNN(backbone, 
+                       num_classes=num_classes)
     
     if parse.mode == "train":
         MODE = TrafficModel(model=model)
 
         train_data = TrafficVehicle(folder="train", transforms=train_transform, transform_box_type="corner")
-        val_data = TrafficVehicle(folder="val", transforms=train_transform, transform_box_type="corner")
+        val_data = TrafficVehicle(folder="val", transforms=test_transform, transform_box_type="corner")
         
         # Load DataLoader
-        train_dataloader = DataLoader(dataset=train_data, batch_size=32, shuffle=True, num_workers=1, collate_fn=collate_fn)
-        val_dataloader = DataLoader(dataset=val_data, batch_size=32, shuffle=True, num_workers=1, collate_fn=collate_fn)
+        train_dataloader = DataLoader(dataset=train_data, batch_size=8, shuffle=True, num_workers=1, collate_fn=collate_fn)
+        val_dataloader = DataLoader(dataset=val_data, batch_size=8, shuffle=True, num_workers=1, collate_fn=collate_fn)
 
         MODE.train(train_data=train_data, 
                    train_dataloader=train_dataloader, 
