c7295e10ee3801ed0a0bd08879a7116b17793402
QuanNguyen123 <128344377+nguyenquan132@users.noreply.github.com>
2025-01-15 15:39:10 +0700

    update model.py

--- faster_rcnn/model.py
+++ faster_rcnn/model.py
@@ -2,7 +2,7 @@ from evaluate import evaluate
 from .train import train_step
 import numpy as np
 from torch import nn
-from function import loss_mAP_curve, precision_recall_curve, confidence_metric
+from function import loss_mAP_curve
 import torch
 import cv2
 import supervision as sv
@@ -47,23 +47,27 @@ class TrafficModel():
         self.model = model
     def train(self, train_data, train_dataloader, val_dataloader, epochs, momentum=0, weight_decay=0):
         # Đóng băng các parameters
-        for param in self.model.parameters():
+        for param in self.model.backbone.parameters():
             param.requires_grad = False
 
-        # Finetuning model 
-        in_features = self.model.roi_heads.box_predictor.cls_score.in_features
-        num_class = 5 # 4 object + 1 background class
-        self.model.roi_heads.box_predictor.cls_score = nn.Linear(in_features=in_features, out_features=num_class)
-        self.model.roi_heads.box_predictor.bbox_pred = nn.Linear(in_features=in_features, out_features=num_class * 4)
-        class_name = train_data.class_name
+        num_classes = 5
 
         # Thiết lập optimizer, device
-        optimizer = torch.optim.SGD(params=self.model.parameters(), lr=0.01, 
-                                    momentum=momentum, weight_decay=weight_decay)
+        params = [p for p in self.model.parameters() if p.requires_grad]
+        optimizer = torch.optim.SGD(
+            params,
+            lr=0.005,
+            momentum=0.9,
+            weight_decay=0.0005
+        )
+        lr_scheduler = torch.optim.lr_scheduler.StepLR(
+            optimizer,
+            step_size=3,
+            gamma=0.1
+        )
         device = "cuda" if torch.cuda.is_available() else "cpu"
 
         # Bắt đầu quá trình training
-        print(f"-----------------------Training-----------------------\n")
         results = {
             "epoch_value": [],
             "loss": [],
@@ -73,57 +77,58 @@ class TrafficModel():
             "loss_rpn_box_reg": [],
             "mAP50": []
         }
-        metrics = {
-            "precisions per class": {class_id: [] for class_id in range(1, num_class)},  
-            "recalls per class": {class_id: [] for class_id in range(1, num_class)},    
-        }
+        epochs = 20
+
+        patience = 3  # Số epoch không cải thiện mAP
+        best_mAP = 0  # Lưu mAP tốt nhất
+        patience_counter = 0  # Đếm số epoch mà mAP không cải thiện
+
         for epoch in range(epochs):
-            print(f"Epoch {epoch + 1}/{1}")
+            print(f"Epoch {epoch + 1}/{epochs}")
             loss, loss_classifier, loss_box_reg, loss_objectness, loss_rpn_box_reg = train_step(train_dataloader=train_dataloader,
-                                                                                                model=self.model,
+                                                                                                model=model,
                                                                                                 optimizer=optimizer,
                                                                                                 device=device)
-            mAP, AP_per_class, precisions_per_class, recalls_per_class = evaluate(val_dataloader=val_dataloader,
-                                                                                model=self.model,
-                                                                                num_class=num_class-1,
-                                                                                iou_threshold=0.5,
-                                                                                device=device)
+
+            lr_scheduler.step()
             
+            mAP, AP_per_class = evaluate(val_dataloader=val_dataloader,
+                                        model=self.model,
+                                        num_class=num_classes-1,
+                                        iou_threshold=0.5,
+                                        device=device)
+
             results["epoch_value"].append(epoch + 1)
-            results["loss"].append(loss.item if isinstance(loss, torch.Tensor) else loss)
-            results["loss_classifier"].append(loss_classifier.item if isinstance(loss_classifier, torch.Tensor) else loss_classifier)
-            results["loss_box_reg"].append(loss_box_reg.item if isinstance(loss_box_reg, torch.Tensor) else loss_box_reg)
-            results["loss_objectness"].append(loss_objectness.item if isinstance(loss_objectness, torch.Tensor) else loss_objectness)
-            results["loss_rpn_box_reg"].append(loss_rpn_box_reg.item if isinstance(loss_rpn_box_reg, torch.Tensor) else loss_rpn_box_reg)
+            results["loss"].append(loss.item() if isinstance(loss, torch.Tensor) else loss)
+            results["loss_classifier"].append(loss_classifier.item() if isinstance(loss_classifier, torch.Tensor) else loss_classifier)
+            results["loss_box_reg"].append(loss_box_reg.item() if isinstance(loss_box_reg, torch.Tensor) else loss_box_reg)
+            results["loss_objectness"].append(loss_objectness.item() if isinstance(loss_objectness, torch.Tensor) else loss_objectness)
+            results["loss_rpn_box_reg"].append(loss_rpn_box_reg.item() if isinstance(loss_rpn_box_reg, torch.Tensor) else loss_rpn_box_reg)
 
             print(f"Loss: {loss:.4f}, Loss classifier: {loss_classifier:.4f}, Loss box: {loss_box_reg:.4f}, Loss objectness: {loss_objectness:.4f}, Loss rpn_box: {loss_rpn_box_reg:.4f}")
 
-            for class_id in range(1, num_class):
-                # Tính giá trị trung bình của precision, recall cho từng class 
-                mean_precision = np.mean(list(precisions_per_class[class_id]))
-                mean_recall = np.mean(list(recalls_per_class[class_id]))
-                
-                metrics["precisions per class"][class_id].append(mean_precision)
-                metrics["recalls per class"][class_id].append(mean_recall)
+            for class_id in range(1, num_classes):
+                class_ap = AP_per_class.get(class_id, 0)
+                print(f"Average Precision of {train_data.class_name[class_id]}: {class_ap:.4f}")
 
-                print(f"Average Precision of {train_data.class_name[class_id]}: {AP_per_class[class_id]}")
-            
-            # Calculate overall mAP
-            mAP = np.mean(list(AP_per_class.values()))
             results["mAP50"].append(mAP)
 
             print(f"Mean Average Precision (mAP@IoU=0.5): {mAP:.4f}")
 
+            # Early stopping check
+            if mAP > best_mAP:
+                best_mAP = mAP
+                patience_counter = 0  # Reset counter nếu mAP không cải thiện
+                # Save model
+                torch.save(obj=self.model.state_dict())
+            else:
+                patience_counter += 1
+                if patience_counter >= patience:
+                    print(f"Early stopping at epoch {epoch + 1} due to no improvement in mAP.")
+                    break  # Dừng huấn luyện khi không cải thiện mAP trong nhiều epoch
+
         # Visualize loss và mAP 
         loss_mAP_curve(results)
-        # Visualize precision-recall curve
-        precision_recall_curve(metrics=metrics, num_class=num_class-1, class_name=class_name)
-        # Visualize precision-confidence curve
-        confidence_metric(metric=metrics["precisions per class"], num_class=num_class-1,
-                        class_name=class_name, metric_name="Precision")
-        # Visualize recall-confidence curve
-        confidence_metric(metric=metrics["recalls per class"], num_class=num_class-1,
-                        class_name=class_name, metric_name="Recall")
     
     def test_one_image(self, img, score_threshold):
         with torch.inference_mode():
