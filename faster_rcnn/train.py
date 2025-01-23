9b8101d89de68eaec02010f429c2eb17b4bc868f
QuanNguyen123 <128344377+nguyenquan132@users.noreply.github.com>
2025-01-24 00:43:57 +0700

    update train.py

--- faster_rcnn/train.py
+++ faster_rcnn/train.py
@@ -6,17 +6,26 @@ from tqdm.auto import tqdm
 def train_step(train_dataloader: DataLoader,
                model: torchvision.models,
                optimizer: torch.optim,
-               device: torch.device):
+               device: torch.device,
+               model_dce=None):
     model = model.to(device)
     model.train()
+    if model_dce is not None: model_dce.eval()
     loss = 0
     loss_classifier, loss_box_reg, loss_objectness, loss_rpn_box_reg = 0, 0, 0, 0
 
     for batch, (images, targets) in tqdm(enumerate(train_dataloader), total=len(train_dataloader)):
         image = list(image.to(device) for image in images)
+        if model_dce is not None:
+            enhanced_images = []
+            for image in images:
+                _, enhance_image, _ = model_dce(image.unsqueeze(0))
+                enhanced_images.append(enhance_image.squeeze())
         target = [{k: v.to(device) for k, v in t.items()} for t in targets]
 
-        loss_dict = model(image, target)
+        if model_dce is not None: loss_dict = model(enhanced_images, target)
+        else: loss_dict = model(image, target)
+        
         if batch == 1:
             print(f"batch 1: \n{loss_dict}")
 
